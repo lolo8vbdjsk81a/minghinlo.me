3972c1594e7d7e6df8c4eba98b572fb7ad60094c
Lo Ming Hin <154828467+lolo8vbdjsk81a@users.noreply.github.com>
2026-07-21 00:44:10 +0800

    Testing CD

--- server.js
+++ server.js
@@ -1,11 +1,19 @@
 // server.js
-const path = require("path");
-const express = require("express");
+import path from "path";
+import express from "express";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);  // basically pwd at .
+
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.static(path.join(__dirname, "public")));
+// runs on every request
+app.use(express.static(path.join(__dirname, "public")));  // /.../public
+// Every request → check if file exists in public/ → if yes serve it → if no, pass to next handler 
 
+// GET: match any URL path, send a file back to the browser
 app.get("/*all", (req, res) => {
 	res.sendFile(path.join(__dirname, "public", "index.html"));
 });
